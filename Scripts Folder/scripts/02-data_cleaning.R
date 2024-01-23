4434fed17e068a513fb20f47b33f51058fb209a5
Tianen (Evan) Hao <evan4830@gmail.com>
2024-01-23 16:52:04 -0500

    Update 02-data_cleaning.R

--- Scripts Folder/scripts/02-data_cleaning.R
+++ Scripts Folder/scripts/02-data_cleaning.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Clean Dinesafe and CPI datas from OpenDataToronto
+# Purpose: Clean Dinesafe and CPI dataset
 # Author: Tianen (Evan) Hao
 # Date: 14 January 2024
 # Contact: evan4830@gmail.com
