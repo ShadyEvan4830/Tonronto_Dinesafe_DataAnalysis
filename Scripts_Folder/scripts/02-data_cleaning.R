4b20eab7a81d21dc7cfa9ee6fe657d7dcf357de3
Tianen (Evan) Hao <evan4830@gmail.com>
2024-01-24 21:15:41 -0500

    Update 02-data_cleaning.R

--- Scripts_Folder/scripts/02-data_cleaning.R
+++ Scripts_Folder/scripts/02-data_cleaning.R
@@ -10,21 +10,21 @@
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read.csv("Inputs Folder/Data/raw_data.csv")
+raw_data <- read.csv("Inputs_Folder/Data/raw_data.csv")
 
 cleaned_data <- dataset %>%
   filter(!is.na(`Establishment Name`)) %>%
   mutate(new_column = tolower(`Establishment Name`))
 
 #### Save data ####
-write.csv(dataset, "Inputs Folder/Data/Clean_Dinesafe_Data.csv")
+write.csv(dataset, "Inputs_Folder/Data/Clean_Dinesafe_Data.csv")
 
 #### Clean data ####
-raw_data2 <- read.csv("Inputs Folder/Data/raw_data2.csv")
+raw_data2 <- read.csv("Inputs_Folder/Data/raw_data2.csv")
 
 cpi_data <- filter(data, measure_name == "CPI - Consumer Price Index - Toronto (% Change)")
 
 cpi_data_renamed <- rename(cpi_data, CPI = measure_value)
 
 #### Save data ####
-write.csv(cpi_data, "Inputs Folder/Data/Clean_CPI_Data.csv")
+write.csv(cpi_data, "Inputs_Folder/Data/Clean_CPI_Data.csv")
